9914577e4f32b8cc1528dbfede8f8406c24be026
Jasmin Kaset <kasetski@gmail.com>
2020-11-22 16:56:16 -0600

    first chapter, organizing module structure

---
+++ src/components/employee/Employee.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employee.css"
+
+export const Employee = () => (
+    <section className="employee">
+        <h3 className="employee__name">Emma Beaton</h3>
+        <div className="employee__branch">Branch: Nashville Kennels North</div>
+    </section>
+)
