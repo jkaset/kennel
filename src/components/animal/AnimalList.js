bc657de70802735f7934eabde1d8892910985076
Jasmin Kaset <kasetski@gmail.com>
2020-12-02 11:02:49 -0600

    search bar on animal

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,48 +1,93 @@
-import React, { useContext, useEffect } from "react"
+// import React, { useState, useContext, useEffect } from "react"
+// import { AnimalContext } from "./AnimalProvider"
+// import Animal from "./Animal"
+// import "./Animal.css"
+
+// export const AnimalList = ({ history }) => {
+//   const { getAnimals, searchTerms, animals } = useContext(AnimalContext)
+
+//   const [ filteredAnimals, setFiltered ] = useState([])
+//   // Initialization effect hook -> Go get animal data
+//   useEffect(() => {
+//     getAnimals()
+//   }, [])
+
+//   useEffect(() => {
+//     if (searchTerms !== "") {
+//         // If the search field is not blank, display matching animals
+//         const subset = animals.filter(animal => animal.name.toLowerCase().includes(searchTerms))
+//         setFiltered(subset)
+//     } else {
+//         // If the search field is blank, display all animals
+//         setFiltered(animals)
+//     }
+// }, [searchTerms, animals])
+
+//   return (
+//     <>
+//       <h1>Animals</h1>
+
+//       <button onClick={() => history.push("/animals/create")}>
+//         Make Reservation
+//           </button>
+//       <div className="animals">
+//         {
+//           animals.map(animal => {
+//             return <Animal key={animal.id} animal={animal} />
+//           })
+//         }
+//       </div>
+//     </>
+//   )
+// }
+
+import React, { useState, useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider"
 import Animal from "./Animal"
 import "./Animal.css"
 
 export const AnimalList = ({ history }) => {
-  const { getAnimals, animals } = useContext(AnimalContext)
+    const { animals, searchTerms, getAnimals } = useContext(AnimalContext)
 
-  // Initialization effect hook -> Go get animal data
-  useEffect(() => {
-    getAnimals()
-  }, [])
+    /*
+        Since you are no longer ALWAYS going to be displaying all animals
+    */
+    const [ filteredAnimals, setFiltered ] = useState([])
 
-  return (
-    <>
-      <h1>Animals</h1>
+    useEffect(() => {
+        getAnimals()
+    }, [])
 
-      <button onClick={() => history.push("/animals/create")}>
-        Make Reservation
-          </button>
-      <div className="animals">
-        {
-          animals.map(animal => {
-            return <Animal key={animal.id} animal={animal} />
-          })
+    /*
+        This effect hook function will run when the following two state changes happen:
+            1. The animal state changes. First when it is created, then once you get the animals from the API
+            2. When the search terms change, which happens when the user types something in the AnimalSearch component
+    */
+    useEffect(() => {
+        if (searchTerms !== "") {
+            // If the search field is not blank, display matching animals
+            const subset = animals.filter(animal => animal.name.toLowerCase().includes(searchTerms))
+            setFiltered(subset)
+        } else {
+            // If the search field is blank, display all animals
+            setFiltered(animals)
         }
-      </div>
-    </>
-  )
+    }, [searchTerms, animals])
+
+    return (
+        <>
+            <h1>Animals</h1>
+
+            <button onClick={() => history.push("/animals/create")}>
+                Make Reservation
+            </button>
+            <div className="animals">
+                {
+                    filteredAnimals.map(animal => {
+                        return <Animal key={animal.id} animal={animal} />
+                    })
+                }
+            </div>
+        </>
+    )
 }
-
-
-
-
-
-    // <div className="animals">
-    //   {
-    //     animals.map(animal => {
-    //       const owner = customers.find(c => c.id === animal.customerId)
-    //       const clinic = locations.find(l => l.id === animal.locationId)
-
-    //       return <Animal key={animal.id}
-    //         location={clinic}
-    //         customer={owner}
-    //         animal={animal} />
-    //     })
-    //   }
-    // </div>
